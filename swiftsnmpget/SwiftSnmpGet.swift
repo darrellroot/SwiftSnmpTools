551720b3c0d46faaccebdae43a9a4760fc1daa02
Darrell Root <feedback@networkmom.net>
2022-07-12 20:47:07 -0700

    This version works

--- swiftsnmpget/SwiftSnmpGet.swift
+++ swiftsnmpget/SwiftSnmpGet.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import Network
+//import Network
 import ArgumentParser
 import SwiftSnmpKit
 
@@ -20,55 +20,25 @@ struct SwiftSnmpGet: ParsableCommand {
     https://github.com/darrellroot/SwiftSnmpKit
     """
     static let configuration = CommandConfiguration(commandName: commandName, abstract: "", usage: "\(commandName) [OPTIONS] AGENT OID", discussion: discussion, version: version, shouldDisplay: true, subcommands: [], defaultSubcommand: nil, helpNames: nil)
-    @Option(name: .short, help: "SNMP community") var community: String
-    @Argument(help: "SNMP agent IP or hostname") var agent: String
-    @Argument(help: "SNMP OID") var oid: String
+    @Option(name: .short, help: "SNMP community") var community: String = "public"
+    @Argument(help: "SNMP agent IP or hostname") var agent: String = "192.168.4.120"
+    @Argument(help: "SNMP OID") var oid: String = "1.3.6.1.2.1.1.1.0"
     
     func run() {
-        var connection: NWConnection
-        connection = NWConnection(host: NWEndpoint.Host(agent), port: 161, using: .udp)
-        let command = SnmpPduType.getRequest
-        let snmpMessage = SnmpMessage(community: community, command: command, oid: SnmpOid(oid)!)
-        let data = snmpMessage.asnData
-        
-        connection.stateUpdateHandler = { (newState) in
-            switch(newState) {
-            case .ready:
-                print("ready")
-                send(connection: connection, data: data)
-                receive(connection: connection)
-            case .setup:
-                print("setup")
-            case .cancelled:
-                print("cancelled")
-            case .preparing:
-                print("Preparing")
-            case .waiting(_):
-                print("waiting")
-            case .failed(_):
-                print("failed")
-            }
+        guard let snmpOid = SnmpOid(oid) else {
+            fatalError("Invalid OID")
         }
-        connection.start(queue: .global())
+        guard let snmpSender = SnmpSender.shared else {
+            fatalError("Snmp Sender not inialized")
+        }
+        do {
+            let result = try snmpSender.snmpGet(host: agent,community: community,oid: snmpOid)
+            print(result)
+        } catch {
+            debugPrint("Unable to send snmpGet: \(error.localizedDescription)")
+        }
         RunLoop.main.run()
     }
-    func send(connection: NWConnection, data: Data) {
-        connection.send(content: data, completion: NWConnection.SendCompletion.contentProcessed(({(error) in
-            print(error?.localizedDescription ?? "no error")
-        })))
-    }
-    func receive(connection: NWConnection) {
-        connection.receiveMessage { (data,context,isComplet,error) in
-            print("Got it")
-            guard let data = data else {
-                fatalError("no data")
-            }
-            guard let snmpMessage = SnmpMessage(data: data) else {
-                fatalError("Failed to decode snmp message")
-            }
-            print(snmpMessage)
-            SwiftSnmpGet.exit()
-        }
-    }
+
 }
 
