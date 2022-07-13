09a7c81e4a1e562b6923044c595d9a3e6f919c1e
Darrell Root <feedback@networkmom.net>
2022-07-13 15:13:24 -0700

    timeouts work!

--- swiftsnmpget/SwiftSnmpGet.swift
+++ swiftsnmpget/SwiftSnmpGet.swift
@@ -11,7 +11,7 @@ import ArgumentParser
 import SwiftSnmpKit
 
 @main
-struct SwiftSnmpGet: ParsableCommand {
+struct SwiftSnmpGet: AsyncParsableCommand {
     // ./swiftsnmpget -c public 192.168.4.120 1.3.6.1.2.1.1.1.0
     static let version = "0.0.2"
     static let commandName = "swiftsnmpget"
@@ -24,7 +24,7 @@ struct SwiftSnmpGet: ParsableCommand {
     @Argument(help: "SNMP agent IP or hostname") var agent: String = "192.168.4.120"
     @Argument(help: "SNMP OID") var oid: String = "1.3.6.1.2.1.1.1.0"
     
-    func run() {
+    func run() async {
         guard let snmpOid = SnmpOid(oid) else {
             fatalError("Invalid OID")
         }
@@ -32,22 +32,23 @@ struct SwiftSnmpGet: ParsableCommand {
             fatalError("Snmp Sender not inialized")
         }
         
-        Task {
-            do {
-                let result = try await snmpSender.snmpGet(host: agent,community: community,oid: snmpOid)
-                
-                switch result {
-                case .failure(let error):
-                    print("SNMP Error: \(error.localizedDescription)")
-                case .success(let variableBinding):
-                    print(variableBinding)
-                }
-                SwiftSnmpGet.exit()
-            } catch {
-                debugPrint("Unable to send snmpGet: \(error.localizedDescription)")
-            }
+        let result = await snmpSender.snmpGet(host: agent,community: community,oid: snmpOid)
+            
+        switch result {
+        case .failure(let error):
+            print("SNMP Error: \(error.localizedDescription)")
+        case .success(let variableBinding):
+            print(variableBinding)
         }
-        RunLoop.main.run()
+        //}
+        /*Task {
+            sleep(10)
+            snmpTask.cancel()
+            print("exiting timer")
+            SwiftSnmpGet.exit()
+        }*/
+        //_ = snmpTask
+        //RunLoop.main.run()
     }
 
 }
